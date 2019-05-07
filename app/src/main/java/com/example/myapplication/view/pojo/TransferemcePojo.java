392c728c8e7dee6f01f39248a4f261903f006c41
Android Studio <devandroid@ort>
2019-05-06 21:06:11 -0300

    charly | POJO clases were created

---
+++ app/src/main/java/com/example/myapplication/view/pojo/TransferemcePojo.java
@@ -0,0 +1,75 @@
+package com.example.myapplication.view.pojo;
+
+import java.util.Date;
+
+public class TransferemcePojo implements MovementPojo {
+
+    private Long id;
+    private Date date;
+    private Float amount;
+    private AccountPojo accountOrigin;
+    private AccountPojo accountDestiny;
+    private String Description;
+
+
+    public TransferemcePojo() {
+    }
+
+    public TransferemcePojo(Long id, Date date, Float amount, AccountPojo accountOrigin, AccountPojo accountDestiny, String description) {
+        this.id = id;
+        this.date = date;
+        this.amount = amount;
+        this.accountOrigin = accountOrigin;
+        this.accountDestiny = accountDestiny;
+        Description = description;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Float amount) {
+        this.amount = amount;
+    }
+
+    public AccountPojo getAccountOrigin() {
+        return accountOrigin;
+    }
+
+    public void setAccountOrigin(AccountPojo accountOrigin) {
+        this.accountOrigin = accountOrigin;
+    }
+
+    public AccountPojo getAccountDestiny() {
+        return accountDestiny;
+    }
+
+    public void setAccountDestiny(AccountPojo accountDestiny) {
+        this.accountDestiny = accountDestiny;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
