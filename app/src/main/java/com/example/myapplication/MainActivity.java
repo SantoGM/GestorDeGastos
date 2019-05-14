466a4605f219d8dd6220edd5615b48bc1183a017
Santiago Suarez <suarez_s@hotmail.com>
2019-05-14 20:50:09 -0300

    Bifurcación de pantalla de inicio para Log In o SignUp según corresponda, actividad base creada.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,20 +2,16 @@ package com.example.myapplication;
 
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.example.myapplication.database.DataBaseContract;
 import com.example.myapplication.database.OpenHelper;
 import com.example.myapplication.database.dataManager.UserDataManager;
-import com.example.myapplication.view.LoginActivity;
 import com.example.myapplication.view.PINActivity;
 import com.example.myapplication.view.RegisterActivity;
 import com.example.myapplication.view.pojo.UserDataPojo;
@@ -28,8 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Toolbar toolbar = findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
