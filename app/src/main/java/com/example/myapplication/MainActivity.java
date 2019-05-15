18bbc758333e3fc5798f0010ad5f221fb683a8a2
SantoGM <suarez_s@hotmail.com>
2019-05-15 00:16:46 -0300

    Se agregan íconos de aplicación

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,20 +2,15 @@ package com.example.myapplication;
 
 import android.content.ContentValues;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.example.myapplication.database.DataBaseContract;
 import com.example.myapplication.database.OpenHelper;
 import com.example.myapplication.database.dataManager.UserDataManager;
-import com.example.myapplication.view.LoginActivity;
 import com.example.myapplication.view.PINActivity;
 import com.example.myapplication.view.RegisterActivity;
 import com.example.myapplication.view.pojo.UserDataPojo;
