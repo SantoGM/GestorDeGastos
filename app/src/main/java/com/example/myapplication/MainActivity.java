efc0df02f27dff15b30ce81f07ea5d5f4a6fbcb8
Santiago Suarez <suarez_s@hotmail.com>
2019-05-20 21:19:05 -0300

    Bifurcación de pantalla de inicio para Log In o SignUp según corresponda, actividad base creada.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,15 +1,8 @@
 package com.example.myapplication;
 
-import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import com.example.myapplication.database.OpenHelper;
 import com.example.myapplication.database.dataManager.UserDataManager;
 import com.example.myapplication.view.IntentViewConstants;
 import com.example.myapplication.view.PINActivity;
